99869fd48d5cdd5e3a754bca6a5414396cfb0c7d
german <germangarau05@gmail.com>
2023-04-22 13:12:11 +0200

    scripts

--- divisio.sh
+++ divisio.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+read -p "introdueix el valor de X: " X
+read -p "introdueix el valor de Y: " Y
+
 A=${1}
 B=${2}
-DIVISIO=$((A/B))
-echo "${A}/${B}"=${DIVISIO}
+DIVISIO=$((X/Y))
+echo "${X}*${Y}=${DIVISIO}"
