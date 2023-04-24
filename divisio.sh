14e0cc9f6fa6e098ea8f0dbab092e1e4ecd65c46
german <germangarau05@gmail.com>
2023-04-24 09:38:36 +0200

    scripts_if

--- divisio.sh
+++ divisio.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#control d'errades
+#sino ens passen dos parametres donam missatge d'errada i sortim de l'escript
+NUM_PARAM=${#}
+echo "NUM_PARAM ${NUM_PARAM}"
+if [[ ${NUM_PARAM} -ne 2 ]]
+then 
+    echo "ERROR: has d'introduir dos parametres"
+    exit 1
+fi
 
-read -p "introdueix el valor de X: " X
-read -p "introdueix el valor de Y: " Y
-
-A=${1}
-B=${2}
+X=${1}
+Y=${2}
 DIVISIO=$(echo "scale=2; ${X}/${Y}" | bc)
 echo "${X}*${Y}=${DIVISIO}"
--- producte.sh
+++ producte.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#control d'errades
+#sino ens passen dos parametres donam missatge d'errada i sortim de l'escript
+NUM_PARAM=${#}
+echo "NUM_PARAM ${NUM_PARAM}"
+if [[ ${NUM_PARAM} -ne 2 ]]
+then 
+    echo "ERROR: has d'introduir dos parametres"
+    exit 1
+fi
 
 A=${1}
 B=${2}
